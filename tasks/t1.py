3ca1bbd0ac48137a544c3fb33ced55883616140e
umar aslam <umarasalam.lhr1@gmail.com>
2024-11-27 11:42:53 +0500

    new changes

--- tasks/t1.py
+++ tasks/t1.py
@@ -4,7 +4,7 @@ from constants import  INDEX_LOOKUP
 
 def get_minimum_temperature(year):
     min = float('inf')
-    data=get_file_contents_by_year(year)
+    data = get_file_contents_by_year(year)
     for row in data:
         if row[INDEX_LOOKUP['min_temperature']] != "" and min > float(row[INDEX_LOOKUP['min_temperature']]):
             min = float(row[INDEX_LOOKUP['min_temperature']])
--- tasks/t2.py
+++ tasks/t2.py
@@ -3,7 +3,7 @@ from constants import  INDEX_LOOKUP
 
 def get_maximum_temperature(year):
     max_value = float('-inf')
-    data=get_file_contents_by_year(year)
+    data = get_file_contents_by_year(year)
     for row in data:
         if row[INDEX_LOOKUP['max_temperature']] != "" and max_value < float(row[INDEX_LOOKUP['max_temperature']]):
             max_value = float(row[1])
