123b7e82b1a98e095b22ef1ad205bfb2e6be1d50
umar aslam <umarasalam.lhr1@gmail.com>
2024-11-27 11:34:24 +0500

    new changes

--- tasks/t4.py
+++ tasks/t4.py
@@ -3,7 +3,7 @@ from constants import  INDEX_LOOKUP
 
 def get_unique_events_in_files():
     events = set()
-    data=get_all_files_contents()
+    data = get_all_files_contents()
     for row in data:
         if row[INDEX_LOOKUP['unique_events']] != "":
             events.add(row[INDEX_LOOKUP['unique_events']])
